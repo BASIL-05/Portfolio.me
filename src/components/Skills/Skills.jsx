8a5650691336783e987b33824300c1f5b62efe35
BASIL SABU <basilsabu5556@gmail.com>
2023-10-28 12:33:56 +0530

    Create Skills.jsx

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,20 @@
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import React from "react";
+import "./style.css";
+
+export const Skills = () => {
+  return (
+    <div className="skills">
+      <p className="text-wrapper-11">What I am capable of?</p>
+      <img className="ps" alt="Ps" />
+      <img className="flutter" alt="Flutter" />
+      <img className="html" alt="Html" />
+      <img className="css" alt="Css" />
+      <img className="js" alt="Js" />
+    </div>
+  );
+};
