3f622db273f2c9cb81d418e85900a544e85708dd
BASIL SABU <basilsabu5556@gmail.com>
2023-10-28 12:32:26 +0530

    Create Projects.stories.js

---
+++ src/components/Projects/Projects.stories.js
@@ -0,0 +1,13 @@
+import { Projects } from ".";
+
+export default {
+  title: "Components/Projects",
+  component: Projects,
+};
+
+export const Default = {
+  args: {
+    className: {},
+    overlapGroupClassName: {},
+  },
+};
